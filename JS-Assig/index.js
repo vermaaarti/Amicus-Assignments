82d5809d5d4b4eda4ee88e467e55317f0524d752
Aarti Verma <aarti_verma@amicusad.local>
2023-08-25 20:33:10 +0530

    added assignment info in a text file

--- JS-Assig/index.js
+++ JS-Assig/index.js
@@ -2,22 +2,23 @@
 var globalArray = [];
 
 $(document).ready(function() {
-  $("#myForm").submit(function(event) {
+ // $("#myForm").submit(function(event) {
+    $("#submitButton").click(function(event) {
     event.preventDefault(); // Prevent form submission
 
     //  object to store form values
     var formDataObject = {};
 
     // Iterating through form elements 
-    $(this).find(".dataToAdd").each(function() {
+    $(`#myForm`).find(".dataToAdd").each(function() {
       formDataObject[$(this).attr("name")] = $(this).val();
     });
     
     //console.log(formDataObject);
     globalArray.push(formDataObject);
-    //console.log(globalArray);
+    console.log(globalArray);
     
   });
 });
- console.log(globalArray);
+// console.log(globalArray);
 
