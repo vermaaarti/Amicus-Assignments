c628aec413c70347f652559645b636941b21f660
Aarti Verma <aarti_verma@amicusad.local>
2023-08-30 18:53:19 +0530

    added the mapping with the associated header value

--- JS-Assig/index.js
+++ JS-Assig/index.js
@@ -1,10 +1,39 @@
-
 var globalArray = [];
 
 $(document).ready(function() {
   // Initialize DataTable
  // var dataTable = $('#dataTable').DataTable();
-  let dataTable = new DataTable('#dataTable');
+ // let dataTable = new DataTable('#dataTable');
+
+
+ // Initialize DataTable with column data mapping
+ 
+ var dataTable = $('#dataTable').DataTable({  
+  data: globalArray,
+  // columns: [
+  //   {
+  //     title : 'Id' , data: 'id',
+  //   },
+  //   {
+  //     title : 'Name' ,data: 'department',
+      
+  //   },
+  //   {
+  //     title : 'Department' ,data: 'shift',
+     
+  //   },
+  //   {
+  //     title : 'Shift' ,data: 'name',
+      
+  //   }
+  // ]
+  columns: [
+    { data: 'id', title: 'ID' },       
+    { data: 'name', title: 'Name' },
+    { data: 'department', title: 'Department' },
+    { data: 'shift', title: 'Shift' }
+  ]
+});
 
   $("#submitButton").click(function(event) {
     event.preventDefault(); // Prevent the default button behavior
@@ -19,13 +48,17 @@ $(document).ready(function() {
     //console.log(globalArray);
 
 
+     // Add the new form data directly to the DataTable
+     dataTable.row.add(formDataObject).draw();
+
+
     // when we want to clear the table record each time
    // Clear existing rows and populate the DataTable with new data
-    dataTable.clear();
-    globalArray.forEach(function(data) {
-      dataTable.row.add([data.id, data.name, data.department, data.shift]);
-    });
-    dataTable.draw();
+    // dataTable.clear();
+    // globalArray.forEach(function(data) {
+    //   dataTable.row.add([data.shift, data.department, data.name,  data.id]);
+    // });
+    // dataTable.draw();
 
 
   // when we don't want to clear the table-data each time 
@@ -36,6 +69,129 @@ $(document).ready(function() {
 
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var globalArray = [];
+
+// $(document).ready(function() {
+//   // Initialize DataTable
+//  // var dataTable = $('#dataTable').DataTable();
+//   let dataTable = new DataTable('#dataTable');
+
+//   $("#submitButton").click(function(event) {
+//     event.preventDefault(); // Prevent the default button behavior
+
+//     var formDataObject = {};
+//     $("#myForm .dataToAdd").each(function() {
+//       formDataObject[$(this).attr("name")] = $(this).val();
+//     });
+
+//     globalArray.push(formDataObject);
+//     console.log(formDataObject);
+//     //console.log(globalArray);
+
+
+//     // when we want to clear the table record each time
+//    // Clear existing rows and populate the DataTable with new data
+//     dataTable.clear();
+//     globalArray.forEach(function(data) {
+//       dataTable.row.add([data.shift, data.department, data.name,  data.id]);
+//     });
+//     dataTable.draw();
+
+
+//   // when we don't want to clear the table-data each time 
+//   // Add the new form data directly to the DataTable
+//   // dataTable.row.add([formDataObject.id, formDataObject.name,
+//   //    formDataObject.department, formDataObject.shift]).draw();
+     
+
+//   });
+// });
 
 
 
