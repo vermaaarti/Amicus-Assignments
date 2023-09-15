8aba093d64461a7d5204761c075747de44238f90
Aarti Verma <aarti_verma@amicusad.local>
2023-09-15 20:27:16 +0530

    added new assignment to fetch the data from API via ajax

--- JS-Assig/Assignment-2/test.js
+++ JS-Assig/Assignment-2/test.js
@@ -1,46 +1,103 @@
 
-var globalArray = [];
+  //   const fetchData = document.getElementById("load-data");
 
-$(document).ready(function() {
+  //   fetchData.addEventListener("click", function(){
 
-  var dataTable = $('#dataTable').DataTable({
-    columns: [
-      { data: 'country', title: 'Country' },
-      { data: 'name', title: 'Name' },
-      { data: 'state', title: 'State' }
-    ],
-    lengthChange: false, 
-    searching: false,
-    info: false,        
-    paging: false 
-  });
+  //   fetch("http://universities.hipolabs.com/search?country=India")
+  //   .then(function(resolve){
+
+  //   if(!resolve.ok){
+  //   throw new Error("some error");
+  //   }
+  //   return resolve.json();
+  //   })
+  //   .then(function(myData){
+  //     console.log("do something here", myData);
+
+  // //    var dataTable = $('#dataTable').DataTable({
+  // //   columns: [
+  // //     { data: 'country', title: 'Country' },
+  // //     { data: 'name', title: 'Name' },
+  // //     { data: 'state', title: 'State' }
+  // //   ],
+  // //   lengthChange: false, 
+  // //   searching: false,
+  // //   info: false,        
+  // //   paging: false 
+  // // });
+
+         
+    
+  //   })
+  //   .catch(function(error){
+  //     console.log("some fetching error is there");
+  //   })
+
+  //   })
 
 
-  $("#submitButton").click(function(event) {
-    event.preventDefault();
-
-//     var formDataObject = {};
-
-//     $("#myForm .dataToAdd").each(function() {
-//       var header = $(this).attr("name");
-//       formDataObject[header] = $(this).val();
-
-//    });  
 
 
-    // Add each row to the DataTable
-    globalArray.forEach(function(data) {
-      dataTable.row.add(data);
-    });
 
-    dataTable.draw();
 
-// reset the input field to null
-   // document.getElementById("myForm").reset();
-   // $('.dataToAdd').val('');
-   clearForm();
-  });
+
+
+
+
+//   fetch('http://universities.hipolabs.com/search?country=India', {
+
+//   method: 'GET'
+
+// })
+
+// .then(response => response.json())
+
+// .then(data => {
+
+//   // Handle the data here
+
+//   console.log('Data from API:', data);
+
+// })
+
+// .catch(error => {
+
+//   console.error('Error:', error);
+
+// });
+
+
+
+
+
+
+
+
+
   
 
+  $(function(){
+  $.ajax({  
+    type: 'GET', 
+    url: 'http://universities.hipolabs.com/search?country=India',
+    //  dataType:'jsonp',
+    // cache: false,
+    //     crossDomain: true,
+        // jsonp: 'callback',   
+    success: function (data, status, xhr) {
+      console.log('data: ', data);
 
+      $('#dataTable').DataTable({
+        data: data,
+        columns: [
+              { data: 'country', title: 'Country' },
+              { data: 'name', title: 'Name' },
+              { data: 'state', title: 'State' }
+            ]           
+      }); 
+    },
+    error: function (xhr, status, error) {
+      console.log('there is some error', error);
+          }
+  });  
 });
