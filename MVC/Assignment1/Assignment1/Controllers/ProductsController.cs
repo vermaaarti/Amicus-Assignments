0849a781b0a0ae01220ef649af6fead035eda089
Aarti Verma <aarti_verma@amicusad.local>
2023-09-21 19:58:00 +0530

    added new MVC folder to maintain the .NET MVC core assignment

---
+++ MVC/Assignment1/Assignment1/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class ProductsController: Controller
+    {
+        
+        //GET : ProductsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET : ProductsController/Details/1
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        //GET : ProductsController/Create
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
