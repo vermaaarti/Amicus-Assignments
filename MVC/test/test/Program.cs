39f946afc910f7b59fb9ac96de0dec632e6f93f6
Aarti Verma <aarti_verma@amicusad.local>
2023-10-17 20:25:06 +0530

    added authentication via Azure Active directory

---
+++ MVC/test/test/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using test.Data;
+namespace test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+                        var connectionString = builder.Configuration.GetConnectionString("testContextConnection") ?? throw new InvalidOperationException("Connection string 'testContextConnection' not found.");
+
+                                    builder.Services.AddDbContext<testContext>(options =>
+                options.UseSqlServer(connectionString));
+
+                                                builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<testContext>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+    .AddAzureAD(options => builder.Configuration.Bind("AzureAD", options));
+
+
+
+            // Define the route for authentication
+            builder.Services.AddAuthorization();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+                        app.UseAuthentication();;
+
+            app.UseAuthorization();
+
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
