7900cbd2ad2fa08b6f6ecf759c0ab4be59315abc
Aarti Verma <aarti_verma@amicusad.local>
2023-11-29 13:19:44 +0530

    added code with performance challenge along with complete procedures.

---
+++ MVC/performance-planning-with-performance-challenge/AADTask/AADTask/Models/AllEmployee.cs
@@ -0,0 +1,68 @@
+﻿
+namespace AADTask.Models
+{
+    public class AllEmployee
+    {
+
+        public int EmployeeId { get; set; }
+
+        public string? EmployeeName { get; set; }
+
+
+        public string? EmployeeEmail { get; set; }
+
+   
+        public string? ManagerName { get; set; }
+
+        public string? Department { get; set; }
+
+        public string? PerformanceRating { get; set; }
+
+        public string? PlannerName { get; set; }
+
+        public string? planneremail { get; set; }
+
+
+        public string? performanceChallenges { get; set; }
+     
+
+        public string? StatusOfPlanning { get; set; }
+
+
+
+
+    }
+    public enum Dept
+    {
+        CS,
+        IT
+    }
+    public enum Mngr
+    {
+        Srishti,
+        Ravi,
+        Ruchir
+    }
+    public enum Plnr
+    {
+        Srishti,
+        Ravi,
+        Ruchir
+    }
+    public enum Perfr
+    {
+         Poor,
+        Good,
+        Satisfactory,
+        Excellent
+    }
+    public enum Perch
+    {
+        Training_Required,
+        Was_Is_in_PIP,
+        No_certification,
+        No_challenges
+    }
+
+
+}
---
+++ MVC/performance-planning-with-performance-challenge/AADTask/AADTask/wwwroot/js/employeePlanning.js
@@ -0,0 +1,77 @@
+﻿
+
+
+
+// fn to save employees data by planner when submit button is clicked
+
+function Submit(event) {
+    event.preventDefault();
+
+
+    // Check if all performance ratings are selected
+    var allRatingsSelected = checkAllRatingsSelected();
+    var allChallengeSelected = checkAllChallengesSelected();
+
+    if (allRatingsSelected && allChallengeSelected) {
+        SaveEmployee(event);
+        // If all ratings are selected, proceed with the AJAX request
+        $.ajax({
+            type: 'POST',
+            url: '/Home/UpdateStatusToCompleted',
+            data: { employeeList: globalArray },
+            success: function () {
+                alert('Status of planning changed to Completed.');
+                // changeDataTable();
+                console.log(globalArray);
+                location.reload(true);
+            },
+            error: function (errorThrown, textStatus, xhr) {
+                console.log('Error in Operation');
+            }
+        });
+    } else {
+        // If not all ratings are selected, show an alert
+        alert('Some performance ratings/challenges are not selected. Please select all ratings before submitting.');
+    }
+}
+
+function checkAllRatingsSelected() {
+    var allRatingsSelected = true;
+
+    // Iterate over the global array and check if all ratings are selected
+    globalArray.forEach(function (item) {
+        var performanceRatingDropdown = `#performanceRatingDropdown${item.employeeId}`;
+        var selectedRating = $(performanceRatingDropdown).val();
+
+        // Check if the performance rating is not selected
+        if (selectedRating === "") {
+            allRatingsSelected = false;
+            return false; // Break out of the loop
+        }
+    });
+
+    return allRatingsSelected;
+}
+
+
+
+function checkAllChallengesSelected() {
+    var allChallengeSelected = true;
+
+    // Iterate over the global array and check if all challenges are selected
+    globalArray.forEach(function (item) {
+        var performanceChallengesDropdown = `#performanceChallengesDropdown${item.employeeId}`;
+        var selectedRating = $(performanceChallengesDropdown).val();
+
+        // Check if the performance challenges is not selected
+        if (selectedRating === "") {
+            allChallengeSelected = false;
+            return false; // Break out of the loop
+        }
+    });
+
+    return allChallengeSelected;
+}
+
+
+// fn ends here
