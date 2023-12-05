876892fa3d16204f8bf128bb613342102140aeb1
Aarti Verma <aarti_verma@amicusad.local>
2023-12-05 20:29:22 +0530

    added new task

---
+++ MVC/performance-planning-with-approver-task/AADTask/AADTask/Models/AllEmployee.cs
@@ -0,0 +1,97 @@
+﻿
+namespace AADTask.Models
+{
+    public class AllEmployee
+    {
+
+        public int EmployeeId { get; set; }
+
+        public string? EmployeeName { get; set; }
+
+
+        public string? EmployeeEmail { get; set; }
+
+   
+        public string? ManagerName { get; set; }
+
+        public string? Department { get; set; }
+
+        public string? PerformanceRating { get; set; }
+
+        public string? PlannerName { get; set; }
+
+        public string? planneremail { get; set; }
+
+
+        public string? approver { get; set; }
+
+
+        public string? performanceChallenges { get; set; }
+     
+
+        public string? StatusOfPlanning { get; set; }
+
+
+
+       /* ApprovalTaskId INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeId INT, 
+	PlannerId INT,
+    ApproverId INT,
+	ApprovalStatus NVARCHAR(255),
+	CreatedOn*/
+
+      public int ApprovalTaskId  { get; set; }
+         public int ApproverId { get; set; }
+      public string? ApproverName { get; set; }
+        public int PlannerId { get; set; }
+         public string? ApprovalStatus { get; set; }
+        public string? CreatedOn { get; set; }
+
+
+      
+
+
+
+    }
+    public enum Dept
+    {
+        CS,
+        IT
+    }
+    public enum Mngr
+    {
+        Srishti,
+        Ravi,
+        Ruchir
+    }
+    public enum Plnr
+    {
+        Srishti,
+        Ravi,
+        Ruchir
+    }
+    public enum Perfr
+    {
+         Poor,
+        Good,
+        Satisfactory,
+        Excellent
+    }
+    public enum Perch
+    {
+        Training_Required,
+        Was_Is_in_PIP,
+        No_certification,
+        No_challenges
+    }
+
+    public enum Approvr
+    {
+        Ruchir,
+        Chandan,
+        Rachana
+    }
+
+    
+
+}
---
+++ MVC/performance-planning-with-approver-task/AADTask/AADTask/wwwroot/js/employeePlanning.js
@@ -0,0 +1,116 @@
+﻿
+
+// fn to save employees data by planner when submit button is clicked
+$(document).ready(function () {
+    sendDataToTaskTable();
+});
+function Submit(event) {
+    event.preventDefault();
+
+
+    // Check if all performance ratings are selected
+    var allRatingsSelected = checkAllRatingsSelected();
+    var allChallengeSelected = checkAllChallengesSelected();
+    //var insertDataIntoDataTable = sendDataToTaskTable() ;
+
+    if (allRatingsSelected && allChallengeSelected) {
+        SaveEmployee(event);
+        // If all ratings are selected, proceed with the AJAX request
+        $.ajax({
+            type: 'POST',
+            url: '/Home/UpdateStatusToCompleted',
+            data: { employeeList: globalArray },
+            success: function () {
+                alert('Status of planning changed to InProgress.');
+               
+               // sendDataToTaskTable();
+                
+                console.log("my array "+ globalArray);
+                location.reload(true);
+
+                 },
+            error: function (errorThrown, textStatus, xhr) {
+                console.log('Error in Operation');
+            }
+        });
+
+           } else {
+        // If not all ratings are selected, show an alert
+        alert('Some performance ratings/challenges are not selected. Please select all ratings before submitting.');
+    }
+}
+
+function checkAllRatingsSelected() {
+    var allRatingsSelected = true;
+
+    // Iterate over the global array and check if all ratings are selected
+    globalArray.forEach(function (item) {
+        var performanceRatingDropdown = `#performanceRatingDropdown${item.employeeId}`;
+        var selectedRating = $(performanceRatingDropdown).val();
+
+        // Check if the performance rating is not selected
+        if (selectedRating === "") {
+            allRatingsSelected = false;
+            return false; // Break out of the loop
+        }
+    });
+
+    return allRatingsSelected;
+}
+
+
+
+function checkAllChallengesSelected() {
+    var allChallengeSelected = true;
+
+    // Iterate over the global array and check if all challenges are selected
+    globalArray.forEach(function (item) {
+        var performanceChallengesDropdown = `#performanceChallengesDropdown${item.employeeId}`;
+        var selectedRating = $(performanceChallengesDropdown).val();
+
+        // Check if the performance challenges is not selected
+        if (selectedRating === "") {
+            allChallengeSelected = false;
+            return false; // Break out of the loop
+        }
+    });
+
+    return allChallengeSelected;
+}
+
+// fn ends here
+
+//fn to send data into the task table
+
+function sendDataToTaskTable() {
+   /* $.ajax({
+        type: 'POST',
+        url: '/Home/AddDataIntoTaskTableForResult',
+        data: { employeeList: ApproverArray },
+        success: function () {
+
+            console.log("my array " + ApproverArray + " is this");
+            // location.reload(true);
+            return true;
+
+        },
+        error: function (errorThrown, textStatus, xhr) {
+            console.log('Error in Operation');
+        }
+    });*/
+
+   
+        $.ajax({
+            type: 'POST',
+            url: '/Home/AddDataIntoTaskTableForResult',
+           success: function () {
+                console.log("Data added to ApprovalTask_aarti table successfully.");
+               
+            },
+            error: function (errorThrown, textStatus, xhr) {
+                console.log('Error in operation: ' + errorThrown);
+            }
+        });
+    
+
+}
