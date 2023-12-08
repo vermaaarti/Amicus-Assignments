1f1ce55728f2ad7f444b432d65ed2df3dd1cd8a2
Aarti Verma <aarti_verma@amicusad.local>
2023-12-08 19:56:28 +0530

    added new task named approver and the functionality that submit button should be invisible when the status of planning is InProgress with all the approver tasks

---
+++ MVC/performance-planning-with-send-back-functionality/AADTask/AADTask/Controllers/HomeController.cs
@@ -0,0 +1,568 @@
+﻿using AADTask.Models;
+using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace AADTask.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly string connectionString;
+        public HomeController(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = User.Identities.FirstOrDefault();
+            if (claimsIdentity != null)
+            {
+                var emailClaim = claimsIdentity?.Claims;
+                if (emailClaim != null)
+                {
+                    var data = emailClaim.FirstOrDefault((p) => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").Value;
+
+
+                    ViewData["name"] = data;
+
+                    return View();
+                }
+            }
+
+
+
+            return View();
+        }
+
+
+
+        // fn to get the employees based on admin or planner
+        public DataTable EmployeeData()
+
+        {
+
+            var claimsIdentity = User.Identities.FirstOrDefault();
+            
+                var emailClaim = claimsIdentity?.Claims;
+               
+                    var email = emailClaim.FirstOrDefault((p) => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").Value;
+
+            DataTable returnDataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = connection;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "procedureToGetEmployeeData_aartiL"; // Use the name of your stored procedure
+
+                cmd.Parameters.Add(new SqlParameter("@Email", email));
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+
+            }
+
+            return returnDataTable;
+
+         }
+
+
+
+       public List<AllEmployee> ConvertTableIntoModel(DataTable returnDataTable)
+        {
+            List<AllEmployee> objectList = new List<AllEmployee>();
+
+            foreach (DataRow dr in returnDataTable.Rows)
+            {
+                AllEmployee newObj = new AllEmployee();
+
+                newObj.EmployeeId = Convert.ToInt32(dr["EmployeeId"]);
+                newObj.EmployeeName = dr["EmployeeName"].ToString();
+                newObj.EmployeeEmail = dr["EmployeeEmail"].ToString();
+                newObj.ManagerName = dr["ManagerName"].ToString();
+                newObj.PlannerName = dr["PlannerName"].ToString();
+                newObj.Department = dr["Department"].ToString();
+                newObj.PerformanceRating = dr["PerformanceRating"].ToString();
+                newObj.performanceChallenges = dr["performanceChallenges"].ToString();
+                newObj.StatusOfPlanning = dr["StatusOfPlanning"].ToString();
+                newObj.PlannerId = Convert.ToInt32(dr["PlannerId"]);
+                newObj.ApproverId = Convert.ToInt32(dr["ApproverId"]);
+                newObj.CreatedOn = dr["CreatedOn"].ToString();
+                newObj.ApprovalStatus = dr["ApprovalStatus"].ToString();
+
+                objectList.Add(newObj);
+            }
+            return objectList;
+        }
+
+      
+        // fn to convert data into json
+        public IActionResult JSONEmployeeData()
+        {
+            var Data = ConvertTableIntoModel(EmployeeData());
+            return Ok(Data);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetEmployeeData()
+        {
+             var filteredData = ConvertTableIntoModel(EmployeeData());
+            return View("EmployeeDataList", filteredData);
+        }
+
+
+
+
+        // fn to save data on database when submit button is clicked by the planner
+        public DataTable UpdateEmployeeStatus(List<AllEmployee> employeeList)
+
+        {
+
+            var returnDataTable = new DataTable();
+            var JSONData = JsonConvert.SerializeObject(employeeList);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = connection;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "ChangeStatusToInProgress";  //  ChangeStatusToCompleted_a  // Use the name of your stored procedure
+
+                cmd.Parameters.Add(new SqlParameter("@JsonEmployee", JSONData));
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+
+            }
+            return returnDataTable;
+
+
+        }
+
+
+        // fn to add data into the task table
+        public DataTable AddDataIntoTaskTable(List<AllEmployee> employeeList)
+
+        {
+
+            var claimsIdentity = User.Identities.FirstOrDefault();
+
+            var emailClaim = claimsIdentity?.Claims;
+
+            var email = emailClaim.FirstOrDefault((p) => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").Value;
+
+            var returnDataTable = new DataTable();
+            var JSONData = JsonConvert.SerializeObject(employeeList);
+          
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = connection;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "AddDataIntoApproverTableT"; // Use the name of your stored procedure
+
+                cmd.Parameters.Add(new SqlParameter("@ApproverEmail", email));
+
+                cmd.Parameters.Add(new SqlParameter("@EmpJson", JSONData));
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+
+            }
+
+            return returnDataTable;
+
+        }
+
+        [HttpPost]
+        public IActionResult AddDataIntoTaskTableForResult(List<AllEmployee> employeeList)
+        {
+            var data = AddDataIntoTaskTable(employeeList);
+            if (data != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateStatusToCompleted(List<AllEmployee> employeeList)
+        {
+            
+            var data = UpdateEmployeeStatus(employeeList);
+            
+            if (data != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        // fn ends here
+
+
+
+
+
+        // fn to send the updated array to the database after changing the performance rating 
+        public DataTable UpdatedRawData(List<AllEmployee> employeeList)
+
+        {
+
+            var returnDataTable = new DataTable();
+            var JSONData = JsonConvert.SerializeObject(employeeList);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = connection;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "SaveEmployeeDatasAsDraft_artiA"; // Use the name of your stored procedure
+
+                cmd.Parameters.Add(new SqlParameter("@JsonEmployee", JSONData));
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+
+            }
+            return returnDataTable;
+
+
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdatedData(List<AllEmployee> employeeList)
+        {
+            var data = UpdatedRawData(employeeList);
+            if (data != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        // fn ends here
+
+
+
+        //   method to update employees existing record by clicking on their name and adding new employee 
+        public IActionResult AddEmployeeForm(EmployeeList employeeList)
+        {
+
+            return View(new AllEmployee());
+        }
+      
+
+        public DataTable AddUpdateEmployee(AllEmployee employeeList)
+        {
+            var returnDataTable = new DataTable();
+            var JsonData = JsonConvert.SerializeObject(employeeList);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "InsertOrUpdateEmployee";
+                cmd.Parameters.Add(new SqlParameter("@EmpJson", JsonData));
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+                dataAdp.Fill(returnDataTable);
+                connection.Close();
+            }
+            return returnDataTable;
+        }
+
+        [HttpPost]
+        public IActionResult AddToTable(AllEmployee empList)
+        {
+            AddUpdateEmployee(empList);
+
+           
+            return Ok();
+        }
+
+        public IActionResult EmployeeDetailView(int id)
+        {
+            if (id != 0)
+            {
+                var data = ConvertTableIntoModel(EmployeeData());
+                var filteredData = data.FirstOrDefault(row => row.EmployeeId == id);
+                return View(filteredData);
+
+            }
+            var emptyData = new AllEmployee();
+            return View(emptyData);
+
+        }
+
+        //fn ends here
+
+
+
+        // fn to go to the approver page
+
+         public DataTable ApproverEmployeeData()
+        {
+            var claimsIdentity = User.Identities.FirstOrDefault();
+            var emailClaim = claimsIdentity?.Claims;
+            var email = emailClaim.FirstOrDefault((p) => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").Value;
+
+            DataTable returnDataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "ShowEmployeForApproverM"; // Updated the procedure name
+                cmd.Parameters.Add(new SqlParameter("@EmployeeEmail", email)); // Pass email directly as a parameter
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+            }
+
+            return returnDataTable;
+        }
+
+
+        public List<AllEmployee> ConvertTableIntoModels(DataTable returnDataTable)
+        {
+            List<AllEmployee> objectList = new List<AllEmployee>();
+
+            foreach (DataRow dr in returnDataTable.Rows)
+            {
+                AllEmployee newObj = new AllEmployee();
+
+                newObj.ApprovalTaskId = Convert.ToInt32(dr["ApprovalTaskId"]);
+                newObj.EmployeeName = dr["EmployeeName"].ToString();
+                newObj.PlannerName = dr["PlannerName"].ToString();
+                newObj.ApproverName = dr["ApproverName"].ToString();
+                newObj.ApprovalStatus = dr["ApprovalStatus"].ToString();
+                newObj.CreatedOn = dr["CreatedOn"].ToString();
+
+               
+                objectList.Add(newObj);
+            }
+            return objectList;
+        }
+        
+
+        // fn to convert data into json
+        public IActionResult ApproverJSONEmployeeData()
+        {
+            var Data = ConvertTableIntoModels(ApproverEmployeeData());
+            return Ok(Data);
+        }
+
+
+        //fn ends here
+
+        public IActionResult ApproverView() {
+          //  var data = AddDataIntoTaskTable();
+           
+            return View("ApproverView");
+           }
+
+
+        // fn to update(change) the employee assigned status into approved status
+        public DataTable UpdateEmployeeAssignedStatus(List<AllEmployee> employeeList)
+
+        {
+            var claimsIdentity = User.Identities.FirstOrDefault();
+            var emailClaim = claimsIdentity?.Claims;
+            var email = emailClaim.FirstOrDefault((p) => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").Value;
+
+
+            var returnDataTable = new DataTable();
+            var JSONData = JsonConvert.SerializeObject(employeeList);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = connection;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "ChangeStatusToApprovedI";  //  ChangeStatusToCompleted_a  // Use the name of your stored procedure
+
+                cmd.Parameters.Add(new SqlParameter("@JsonEmployee", JSONData));
+
+                cmd.Parameters.Add(new SqlParameter("@ApproverEmail", email));
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+
+            }
+            return returnDataTable;
+
+
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAssignedStatusToApproved(List<AllEmployee> employeeList)
+        {
+           
+
+            var data = UpdateEmployeeAssignedStatus(employeeList);
+            if (data != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        // fn ends here
+
+
+
+        // fn to update(change) the approvalStatus from assigned to Unassigned
+        public DataTable UpdateAssignedToUnassigned()
+
+        {
+           
+            var returnDataTable = new DataTable();
+            //var JSONData = JsonConvert.SerializeObject();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            {
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = connection;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.CommandText = "ChangeStatusToUnassignedB"; // Use the name of your stored procedure
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(cmd);
+
+                dataAdp.Fill(returnDataTable);
+
+                connection.Close();
+
+            }
+            return returnDataTable;
+
+
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAssignedStatusToUnassigned()
+        {
+
+
+            var data = UpdateAssignedToUnassigned();
+            if (data != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        // fn ends here
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult EmployeeList()
+        {
+
+            return View();
+        }
+
+
+
+       
+
+
+
+
+
+       
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC/performance-planning-with-send-back-functionality/AADTask/AADTask/wwwroot/js/employeePlanning.js
@@ -0,0 +1,126 @@
+﻿
+
+
+
+// fn to save employees data by planner when submit button is clicked
+function Submit(event) {
+    event.preventDefault();
+
+
+    // Check if all performance ratings are selected
+    var allRatingsSelected = checkAllRatingsSelected();
+    var allChallengeSelected = checkAllChallengesSelected();
+
+    if (allRatingsSelected && allChallengeSelected) {
+        SaveEmployee(event);
+        // If all ratings are selected, proceed with the AJAX request
+        $.ajax({
+            type: 'POST',
+            url: '/Home/UpdateStatusToCompleted',
+            data: { employeeList: globalArray },
+            success: function () {
+             
+                alert('Status of planning changed to InProgress.');
+               
+               // sendDataToTaskTable();
+                
+                console.log("my array "+ globalArray);
+                location.reload(true);
+
+                 },
+            error: function (errorThrown, textStatus, xhr) {
+                console.log('Error in Operation');
+            }
+        });
+
+           } else {
+        // If not all ratings are selected, show an alert
+        alert('Some performance ratings/challenges are not selected. Please select all ratings before submitting.');
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/Home/AddDataIntoTaskTableForResult',
+        data: {employeeList:globalArray},
+        success: function () {
+          console.log("Data added to ApprovalTask_aarti table successfully.");
+           
+        },
+        error: function (errorThrown, textStatus, xhr) {
+            console.log('Error in operation: ' + errorThrown);
+        }
+    });
+
+   }
+
+function checkAllRatingsSelected() {
+    var allRatingsSelected = true;
+
+    // Iterate over the global array and check if all ratings are selected
+    globalArray.forEach(function (item) {
+        var performanceRatingDropdown = `#performanceRatingDropdown${item.employeeId}`;
+        var selectedRating = $(performanceRatingDropdown).val();
+
+        // Check if the performance rating is not selected
+        if (selectedRating === "") {
+            allRatingsSelected = false;
+            return false; // Break out of the loop
+        }
+    });
+
+    return allRatingsSelected;
+}
+
+
+
+function checkAllChallengesSelected() {
+    var allChallengeSelected = true;
+
+    // Iterate over the global array and check if all challenges are selected
+    globalArray.forEach(function (item) {
+        var performanceChallengesDropdown = `#performanceChallengesDropdown${item.employeeId}`;
+        var selectedRating = $(performanceChallengesDropdown).val();
+
+        // Check if the performance challenges is not selected
+        if (selectedRating === "") {
+            allChallengeSelected = false;
+            return false; // Break out of the loop
+        }
+    });
+
+    return allChallengeSelected;
+}
+
+// fn ends here
+
+//fn to send data into the task table
+
+/*function sendDataToTaskTable() {
+      
+        $.ajax({
+            type: 'POST',
+            url: '/Home/AddDataIntoTaskTableForResult',
+            success: function () {
+               
+            },
+            error: function (errorThrown, textStatus, xhr) {
+                console.log('Error in operation: ' + errorThrown);
+            }
+        });
+    
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
